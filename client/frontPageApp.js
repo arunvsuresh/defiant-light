6daa7b2bb23e5213f559d7b1f80af721367736d6
Arun Suresh <arunvsuresh@gmail.com>
2015-01-21 21:55:06 -0800

    (feat) adds angularFire to enable firebase communication.

--- client/frontPageApp.js
+++ client/frontPageApp.js
@@ -5,3 +5,16 @@ var frontPageApp = angular.module('frontPage', []);
 	
 
 });
+
+
+// Firebase is dependency in this module
+// Allows access to $firebase service from your controllers, factories, and services.
+
+var myApp = angular.module('frontPageApp', ['firebase']);
+
+.controller('frontPageController', ['$scope', '$firebase', function(){
+	function($scope, $firebase) {
+		var ref = new Firebase('https://rkl88y2k0cy.firebaseio-demo.com/');
+		$scope.messages = $firebase(ref).$asArray();
+	}
+}])
