ddf6847175e6da0348f40962589fca0f81af5abf
Arun Suresh <arunvsuresh@gmail.com>
2015-01-22 16:37:52 -0800

    (feat) Continue scaffolding front page and have confirmation message.

--- client/frontPageApp.js
+++ client/frontPageApp.js
@@ -3,15 +3,34 @@ var validationApp = angular.module('frontPageApp', []);
 
 // create angular controller
 validationApp.controller('frontPageController', function($scope) {
-
+	$scope.showPurchaseTitle = true;
+	$scope.showConfirmationTitle = false;
+	//$scope.formContainer;
 	// function to submit the form after all validation has occurred			
 	$scope.submitForm = function(isValid) {
 
 		// check to make sure the form is completely valid
-		if (isValid) { 
-			alert('our form is amazing');
-		}
+		// if (isValid) { 
+		// 	alert('our form is amazing');
+		// }
+
+		// if username isValid && bitcoinId isValid
+			// serve up confirmation message
+
 
 	};
+
+	$scope.updateConfirmation = function() {
+
+		//var $update = '<div class="updateMessage">' + Please wait while we update the 
+		$scope.showPurchaseTitle = false;
+		$scope.showConfirmationTitle = true;
+
+		//console.log($scope.formContainer);
+		//$scope.formContainer = d;
+
+		//console.log($scope.formContainer);
+
+	}
 
 });
